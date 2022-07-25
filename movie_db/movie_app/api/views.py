857d21702996cb7de3229f2df6dc8522d4da4c58
endiesworld <endiesworld@gmail.com>
2022-07-25 04:59:24 +0100

    Added first model serializer

---
+++ movie_db/movie_app/api/views.py
@@ -0,0 +1,18 @@
+from movie_app.models import Movie
+from movie_app.api.api_serializer import MovieSerializer
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+
+
+@api_view()
+def movie_list(request):
+    movies = Movie.objects.all()
+    serializer = MovieSerializer(movies)
+    return Response(serializer.data)
+
+
+@api_view()
+def movie_detials(request, pk):
+    movie = Movie.objects.get(pk=pk)
+    serializer = MovieSerializer(movie)
+    return Response(serializer.data)
